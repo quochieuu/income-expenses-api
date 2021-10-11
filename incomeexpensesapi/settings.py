6873a1455a3a603f01ca5ba101661df0399eedb3
Hieu Ho <hieu_ho@helloleotech.com>
2021-10-11 16:58:05 -0700

    Add pagination

--- incomeexpensesapi/settings.py
+++ incomeexpensesapi/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
