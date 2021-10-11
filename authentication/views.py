d519b98ae33c5bac2f8c280022934483441d6ec6
Hieu Ho <hieu_ho@helloleotech.com>
2021-10-11 08:23:13 -0700

    Implement sending email

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, status, views, permissions
-from .serializers import RegisterSerializer
+from .serializers import EmailVerificationSerializer, RegisterSerializer
 from .renderers import UserRenderer
 from .models import User
 from .utils import Util
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+import jwt
+from django.conf import settings
 
 
 class RegisterView(generics.GenericAPIView):
@@ -20,6 +22,24 @@ class RegisterView(generics.GenericAPIView):
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
         user_data = serializer.data
-       
+        user = User.objects.get(email=user_data['email'])
+
+        token = RefreshToken.for_user(user).access_token
+        
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = 'Hi '+user.username + \
+            ' Use the link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': 'hoquochieu.vn@gmail.com',
+                'email_subject': 'Verify your email'}
+
+        Util.send_email(data)
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(views.APIView):
+    def get(self):
+        pass
