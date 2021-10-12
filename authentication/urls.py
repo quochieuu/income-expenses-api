4c09daa9023e567c8f5ccedd0c72d136b3232b29
Hieu Ho <hieu_ho@helloleotech.com>
2021-10-11 18:53:31 -0700

    Implement reset password by email

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, VerifyEmail, LoginAPIView
+from .views import PasswordTokenCheckAPI, RegisterView, RequestPasswordResetEmail, SetNewPasswordAPIView, VerifyEmail, LoginAPIView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -9,4 +9,10 @@ urlpatterns = [
     path('login/', LoginAPIView.as_view(), name="login"),
     path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('request-reset-email/', RequestPasswordResetEmail.as_view(),
+         name="request-reset-email"),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordTokenCheckAPI.as_view(), name='password-reset-confirm'),
+    path('password-reset-complete', SetNewPasswordAPIView.as_view(),
+         name='password-reset-complete')
 ]
